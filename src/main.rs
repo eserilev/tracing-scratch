07ba9e828d2ae43da2f239934bf8a5b8140d35ea
Eitan Seri-Levi <eserilev@ucsc.edu>
2024-10-23 12:55:36 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod logging_layer;
 
 use logging_layer::LoggingLayer;
+use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
@@ -9,6 +10,7 @@ fn main() {
         .finish()
         .with(custom_layer)
         .try_init();
+    info!("This comes from tracing_scratch crate");
 
     example_crate::logging_from_another_crate()
 }
