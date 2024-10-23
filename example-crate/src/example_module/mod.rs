237c1b987283e2b4fe3880e994c0612c25577bc7
Eitan Seri-Levi <eserilev@ucsc.edu>
2024-10-23 13:17:14 -0700

    add module

---
+++ example-crate/src/example_module/mod.rs
@@ -0,0 +1,5 @@
+use tracing::info;
+
+pub fn another_log() {
+    info!("Testing a sub module in a crate");
+}
--- example-crate/src/lib.rs
+++ example-crate/src/lib.rs
@@ -1,6 +1,10 @@
+mod example_module;
+use example_module::another_log;
 use tracing::info;
 
 pub fn logging_from_another_crate() {
     info!("Testing a different crate");
+
+    another_log();
 }
 
